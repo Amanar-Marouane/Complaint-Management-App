2c675fa32bd8b55d45d47b19e2a270d71f8d58d0
Amanar Marouane <marouane.amanar07@gmail.com>
2024-09-19 13:13:43 +0200

    Sign Up and informations validators Done

---
+++ Project.c
@@ -0,0 +1,314 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#define MAX_NAME_LENGTH 30
+#define MAX_PHONE_NUMBER_LENGTH 30
+#define MAX_EMAIL_LENGTH 256
+#define MAX_PASSWORD_LENGTH 30
+#define PHONE_NUMBER_LENGTH 10
+
+typedef struct
+{
+    char FullName[MAX_NAME_LENGTH];
+    char UserName[MAX_NAME_LENGTH];
+    char PhoneNumber[MAX_PHONE_NUMBER_LENGTH];
+    char Email[MAX_EMAIL_LENGTH];
+    char PassWord[MAX_PASSWORD_LENGTH];
+    char AccType[MAX_NAME_LENGTH];
+} ACC;
+
+int AccNum = 0;
+int num = 1;
+
+void Sign_Up(ACC *accounts);
+
+int name_validator(int AccNum, ACC *accounts);
+int username_validator(int AccNum, ACC *accounts);
+int num_validator(int AccNum, ACC *accounts);
+int email_validator(int AccNum, ACC *accounts);
+int pass_word_validator(int AccNum, ACC *accounts);
+
+int main()
+{
+    char choix;
+
+    ACC *accounts = malloc(num * sizeof(ACC));
+    if (accounts == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        printf("-------------------------------------\n");
+        return 1;
+    }
+
+    do
+    {
+        printf("Press 0 to quit the program\n");
+        printf("Press 1 to Sign Up.\n");
+        printf("Press 2 to Sign in.\n");
+        printf("==> ");
+        scanf(" %c",&choix);
+        printf("-------------------------------------\n");
+        getchar();
+
+        switch (choix)
+        {
+        case '0':
+            break;
+        case '1':
+            Sign_Up(accounts);
+            AccNum ++;
+            num ++;
+            accounts = realloc(accounts, num * sizeof(ACC));
+            if (accounts == NULL)
+            {
+                printf("Memory allocation failed!\n");
+                printf("-------------------------------------\n");
+                return 1;
+            }   
+            break;
+        default:
+            break;
+        }
+    } while (choix != '0');
+
+    free(accounts);
+    return 0;
+}
+
+void Sign_Up(ACC *accounts)
+{
+    do
+    {
+        printf("Enter your full name : ");
+        fgets(accounts[AccNum].FullName, MAX_NAME_LENGTH, stdin);
+        accounts[AccNum].FullName[strcspn(accounts[AccNum].FullName, "\n")] = '\0';
+    } while (name_validator(AccNum, accounts) == 1);
+    
+
+    do
+    {
+        printf("Enter your UserName : ");
+        fgets(accounts[AccNum].UserName, MAX_NAME_LENGTH, stdin);
+        accounts[AccNum].UserName[strcspn(accounts[AccNum].UserName, "\n")] = '\0';
+        printf("----------------------------------------------------------------\n");
+    } while (username_validator(AccNum, accounts) == 1);
+    
+    do
+    {
+        printf("Enter your phone number : ");
+        fgets(accounts[AccNum].PhoneNumber, MAX_PHONE_NUMBER_LENGTH, stdin);
+        accounts[AccNum].PhoneNumber[strcspn(accounts[AccNum].PhoneNumber, "\n")] = '\0';
+        printf("----------------------------------------------------------------\n");
+    } while (num_validator(AccNum, accounts) == 0);
+    
+    do
+    {
+        printf("Enter your email : ");
+        fgets(accounts[AccNum].Email, MAX_EMAIL_LENGTH, stdin);
+        accounts[AccNum].Email[strcspn(accounts[AccNum].Email, "\n")] = '\0';
+        printf("----------------------------------------------------------------\n");
+    } while (email_validator(AccNum, accounts) == 1);
+
+    do
+    {
+        printf("Enter your password : \nPassword must have \n");
+        printf("- At least 8 characters\n");
+        printf("- One uppercase letter\n");
+        printf("- One lowercase letter\n");
+        printf("- One number\n");
+        printf("- One special character (!@#$%^&*)\n");
+        printf("==> ");
+        fgets(accounts[AccNum].PassWord, MAX_PASSWORD_LENGTH, stdin);
+        accounts[AccNum].PassWord[strcspn(accounts[AccNum].PassWord, "\n")] = '\0';
+        printf("----------------------------------------------------------------\n");
+    } while (pass_word_validator(AccNum, accounts) == 1);
+    
+    strcpy(accounts[AccNum].AccType, "Client");
+
+    printf("Sign-up successful for %s\n", accounts[AccNum].FullName);
+    printf("----------------------------------------------------------------\n");
+}
+
+int name_validator(int AccNum, ACC *accounts)
+{   
+    if (strlen(accounts[AccNum].FullName) == 0)
+    {
+        printf("==>Contact name is not valid .\n(Empty!!)\n");
+        printf("----------------------------------------------------------------\n");
+        return 1;
+    }
+    if (accounts[AccNum].FullName[0] == ' ')
+    {
+        printf("==>Contact name is not valid .\n(Must not start with space!!)\n");
+        printf("----------------------------------------------------------------\n");
+        return 1;
+    }
+    for (int i = 0;accounts[AccNum].FullName[i] != '\0' ; i++)
+    {
+        if (!isalpha(accounts[AccNum].FullName[i]) && !isspace(accounts[AccNum].FullName[i]))
+        {
+            printf("==>Contact name is not valid .\n(Must contain only alphabets!!)\n");
+            printf("----------------------------------------------------------------\n");
+            return 1;
+        }
+    }
+    printf("==>Contact name is accepted !!\n");
+    printf("----------------------------------------------------------------\n");
+    return 0;
+}
+int username_validator(int AccNum, ACC *accounts)
+{
+    if (strlen(accounts[AccNum].UserName) == 0)
+    {
+        printf("==>UserName is not valid .\n(Empty!!)\n");
+        printf("----------------------------------------------------------------\n");
+        return 1;
+    }
+    for (int i = 0;accounts[AccNum].UserName[i] != '\0' ; i++)
+    {
+        if (!isalnum(accounts[AccNum].UserName[i]))
+        {
+            printf("==>UserName is not valid.\n(Must contain only alphabets and digits.)\n");
+            printf("----------------------------------------------------------------\n");
+            return 1;
+        }
+    }
+    printf("==>UserName is accepted !!\n");
+    printf("----------------------------------------------------------------\n");
+    return 0;
+}
+int num_validator(int AccNum, ACC *accounts){
+    for (int i = 0;accounts[AccNum].PhoneNumber[i] != '\0' ; i++)
+    {
+        if (!isdigit(accounts[AccNum].PhoneNumber[i]))
+        {
+            printf("==>Phone number is not valid.\n(Must contain only digits!!)\n");
+            printf("----------------------------------------------------------------\n");
+            return 0;
+        }
+    }
+    if (strlen(accounts[AccNum].PhoneNumber) != PHONE_NUMBER_LENGTH)
+    {
+        printf("==>Phone number is not valid.\n(Must contain 10 digits!!)\n");
+        printf("----------------------------------------------------------------\n");
+        return 0;
+    }
+    printf("==>Phone number is accepted !!\n");
+    printf("----------------------------------------------------------------\n");
+    return 1;
+}
+int email_validator(int AccNum, ACC *accounts){
+    char *at = strchr(accounts[AccNum].Email, '@');
+    char *dot = strrchr(accounts[AccNum].Email, '.');
+
+    if (at == NULL || dot == NULL || dot < at)
+    {
+        printf("==>Email is not valid.\n(Must be in the format username@gmail.com)\n");
+        printf("----------------------------------------------------------------\n");
+        return 1;
+    }
+
+    if (at == accounts[AccNum].Email)
+    {
+        printf("==> The username part before '@' cannot be empty. (username@gmail.com)\n");
+        printf("-----------------------------------------------------------------------\n");
+        return 1;
+    }
+    if (at + 1 == dot)
+    {
+        printf("==> The example part between '@' and '.' cannot be empty. (username@gmail.com)\n");
+        printf("----------------------------------------------------------------\n");
+        return 1;
+    }
+    if (*(dot + 1) == '\0')
+    {
+        printf("==> The domain extension after '.' cannot be empty. (username@gmail.com)\n");
+        printf("----------------------------------------------------------------\n");
+        return 1;
+    }
+    
+    for (int i = 0; i < (at - accounts[AccNum].Email); i++)
+    {
+        if (!(isalnum(accounts[AccNum].Email[i]) || accounts[AccNum].Email[i] == '.' || accounts[AccNum].Email[i] == '-' || accounts[AccNum].Email[i] == '_') && isspace(accounts[AccNum].Email[i]))
+        {
+            printf("==>The username part accept only ('A-Z', 'a-z', '0-9', '.', '-', '_'), Space not include.\n");
+            printf("-----------------------------------------------------------------------\n");
+            return 1;
+        }
+    }
+    for (int i = (at - accounts[AccNum].Email) + 1; i < (dot - accounts[AccNum].Email); i++)
+    {
+        if (!isalpha(accounts[AccNum].Email[i]))
+        {
+            printf("==>Special symbol and numbers not valid atfer '@'.\n");
+            printf("-----------------------------------------------------------------------\n");
+            return 1;
+        }
+    }
+
+    const char* gmail = "gmail";
+    if (strncmp(at + 1, gmail, strlen(gmail)) != 0 || at + 1 + strlen(gmail) != dot) 
+    {   
+        printf("==>Only accept (username@gmail.com).\n");
+        printf("-----------------------------------------------------------------------\n");
+        return 1;
+    }
+
+    for (int i = (dot - accounts[AccNum].Email) + 1; accounts[AccNum].Email[i] != '\0'; i++)
+    {
+        if (!isalpha(accounts[AccNum].Email[i]))
+        {
+            printf("==>Special symbol and numbers not valid in the extension part.\n");
+            printf("-----------------------------------------------------------------------\n");
+            return 1;
+        }
+    }
+
+    const char* com = "com";
+    if (strcmp(dot + 1,com) != 0)
+    {
+        printf("==>Only accept (username@gmail.com).\n");
+        printf("-----------------------------------------------------------------------\n");
+        return 1;
+    }
+
+    printf("==>The email accepted !!\n");
+    printf("-----------------------------------------------------------------------\n");
+    return 0;
+}
+int pass_word_validator(int AccNum, ACC *accounts)
+{
+    int lower = 0, upper = 0, number = 0, special = 0;
+
+    if (strlen(accounts[AccNum].PassWord) < 8)
+    {
+        printf("Password too short! Must be at least 8 characters.\n");
+        printf("----------------------------------------------------------------\n");
+        return 1;
+    }
+    if (strcmp(accounts[AccNum].PassWord, accounts[AccNum].UserName) == 0)
+    {
+        printf("Password should not be the same as the username!\n");
+        printf("----------------------------------------------------------------\n");
+        return 1;
+    }
+    
+    for (int i = 0; accounts[AccNum].PassWord[i] != '\0'; i++)
+    {
+        if (islower(accounts[AccNum].PassWord[i])) lower++;
+        else if (isupper(accounts[AccNum].PassWord[i])) upper++;
+        else if (isdigit(accounts[AccNum].PassWord[i])) number++;
+        else if (strchr("!@#$%^&*", accounts[AccNum].PassWord[i])) special++;
+    }
+    if (lower > 0 && upper > 0 && number > 0 && special > 0) {
+        printf("Password is valid.\n");
+        printf("----------------------------------------------------------------\n");
+        return 0;
+    }
+
+    printf("Password invalid! It must contain at least one lowercase letter, one uppercase letter, one number, and one special character.\n");
+    printf("------------------------------------------------------------------------------------------------------------------------------\n");
+    return 1;
+}
